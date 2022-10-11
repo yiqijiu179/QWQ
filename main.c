525a050786db0a2e9bfdeb905a6f438f7b57435a
MallowYang <114585669+MallowYang@users.noreply.github.com>
2022-10-12 03:15:53 +0800

    Update main.c

--- main.c
+++ main.c
@@ -4,6 +4,6 @@ int main(int argc, char **argv)
 {
 char first[255], last[255];
 askname(first, last);
-printf("Hello, %s %s!\n", first, last);
+printf("Hello and bye, %s %s!\n", first, last);
 return 0;
 }
